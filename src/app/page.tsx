e14d63c48445ea3b763d1788663704a29750c4df
Faris <maulfawriz@gmail.com>
2025-11-17 18:44:12 +0700

    fixed main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Container from "@/components/lib/ui/container";
 
+
 export default function Home() {
   return (
     <Container className="">
-      <h1 className="text-4xl font-bold">Welcome to My Website</h1>
-      <p className="mt-4">This is a simple container component.</p>
+
     </Container>
   );
 }
