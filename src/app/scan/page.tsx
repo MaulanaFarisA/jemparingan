ea8c0ada9643c36b802542b1af6b41296ecfd058
unknown <satyawirapramudita@gmail.com>
2025-11-25 22:56:03 +0700

    gtw2

--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -3,7 +3,7 @@ import ScanUlangPopup from "@/components/lib/ui/scan_ulang_popup";
 import ScanBarcodeHeader from "@/components/lib/ui/scan_barcode_header";
 import TombolManualInput from "@/components/lib/ui/input_manual_tombol";
 import SimpanTombol from "@/components/lib/ui/simpan_tombol";
-import TigaTombol from "@/components/lib/ui/3_tombol";
+import TigaTombol from "@/compone nts/lib/ui/3_tombol";
 import SatuTombol from "@/components/lib/ui/1_tombol";
 
 export default function Page() {
