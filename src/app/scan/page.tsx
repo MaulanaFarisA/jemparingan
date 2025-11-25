2ab471fc832742d5eb91df6dcdaa08bc2c16cb53
unknown <satyawirapramudita@gmail.com>
2025-11-25 22:11:05 +0700

    added simpan button

--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -2,12 +2,13 @@ import DataDisimpanPopup  from "@/components/lib/ui/data_disimpan_popup";
 import ScanUlangPopup from "@/components/lib/ui/scan_ulang_popup";
 import ScanBarcodeHeader from "@/components/lib/ui/scan_barcode_header";
 import TombolManualInput from "@/components/lib/ui/input_manual_tombol";
+import SimpanTombol from "@/components/lib/ui/simpan_tombol";
 // import BarcodeScanner from "@/components/lib/ui/barcode_scanner";
 
 export default function Page() {
   return (
     <main>  
-      
+      <SimpanTombol />
     </main>
   );
 }
