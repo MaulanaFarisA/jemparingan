86588fd4457d66c5508dacb0889a51dc47acd471
unknown <satyawirapramudita@gmail.com>
2025-11-26 11:21:53 +0700

    added previous page function to back button on scanning page

--- src/components/lib/ui/scan_barcode_header.tsx
+++ src/components/lib/ui/scan_barcode_header.tsx
@@ -1,18 +1,33 @@
+"use client";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function ScanBarcodeHeader() {
+  const router = useRouter();
+
   return (
-    <div className="bg-[#AE6924] rounded-[20px] flex items-center" style={{ width: 346, height: 84 }}>
+    <div
+      className="bg-[#AE6924] rounded-[20px] flex items-center"
+      style={{ width: 346, height: 84 }}
+    >
       <div
-        className="bg-white rounded-full flex items-center justify-center"
+        onClick={() => router.back()}
+        className="bg-white rounded-full flex items-center justify-center cursor-pointer"
         style={{
-          width: 33,
-          height: 33,
-          marginLeft: 20,
+          width: 34,
+          height: 34,
+          marginLeft: 21,
         }}
       >
-        <Image src="/chevron-left.svg" alt="Arrow Left" width={20} height={20} />
+        <Image
+          src="/chevron-left.svg"
+          alt="Arrow Left"
+          width={21}
+          height={20}
+        />
       </div>
+
       <span
         className="font-Poppins"
         style={{
